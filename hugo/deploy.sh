7a7a80f643661413672f7ad3fc33d85b0725050e
minorugh <minorugh@gmail.com>
2022-08-28 08:12:12 +0900

    Updated: 2022-08-28 08:12:12

---
+++ hugo/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+hugo
+git add . && git diff --cached --exit-code --quiet && echo "\nnothing to commit, working tree clean!"|| \
+git commit -a -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" && \
+git push origin main
+chromium https://minorugh.github.io/.emacs.d
